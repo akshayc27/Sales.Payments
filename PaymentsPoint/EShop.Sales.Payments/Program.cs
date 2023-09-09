142bfd206d6947656555bea3bbb3d44cd5c22f12
akshayc27 <akshaychacharkar27@gmail.com>
2023-09-09 12:47:32 +0530

    Configuring Logger.

--- PaymentsPoint/EShop.Sales.Payments/Program.cs
+++ PaymentsPoint/EShop.Sales.Payments/Program.cs
@@ -1,13 +1,10 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Sales.Payments;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Events;
+using Serilog.Exceptions.Core;
+using Serilog.Exceptions.EntityFrameworkCore.Destructurers;
+using Serilog.Formatting.Compact;
+using Serilog.Parsing;
 
 namespace Sales.Payments
 {
@@ -15,10 +12,13 @@ namespace Sales.Payments
     {
         public static void Main(string[] args)
         {
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? 
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ??
                 throw new InvalidOperationException("Missing required envirement variable ASPNETCORE_ENVIRONMENT.");
 
+            //Here We would need logger to record any startup errors.
+            //We will overrite it when we configure final web host.
 
+            //Log.Logger = ConfigureSerilog();
 
             CreateHostBuilder(args).Build().Run();
         }
@@ -29,5 +29,23 @@ namespace Sales.Payments
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
+        private static LoggerConfiguration ConfigureSerilog(LoggerConfiguration config, string environment)
+        {
+            //Environment agnostic setup
+            config.Enrich.FromLogContext()
+                .Enrich.WithExceptionDetails(
+                new DestructuringOptionsBuilder().WithDefaultDestructurers()
+                .WithDestructurers(new[] { new DbUpdateExceptionDestructurer() })
+                );
+
+            return environment switch
+            {
+                //"Development" => ConfigureDevelopmentLogging(config),
+                "Staging" => config.WriteTo.Console(formatter: new RenderedCompactJsonFormatter()),
+
+            };
+        }
     }
 }
