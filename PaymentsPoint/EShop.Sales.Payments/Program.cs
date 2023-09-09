141d299320f2c932a5676c5f46897c7add854430
akshayc27 <akshaychacharkar27@gmail.com>
2023-09-09 21:16:50 +0530

    Logging Configured.

--- PaymentsPoint/EShop.Sales.Payments/Program.cs
+++ PaymentsPoint/EShop.Sales.Payments/Program.cs
@@ -5,6 +5,8 @@ using Serilog.Exceptions.Core;
 using Serilog.Exceptions.EntityFrameworkCore.Destructurers;
 using Serilog.Formatting.Compact;
 using Serilog.Parsing;
+using Destructurama;
+using System.Reflection;
 
 namespace Sales.Payments
 {
@@ -18,7 +20,9 @@ namespace Sales.Payments
             //Here We would need logger to record any startup errors.
             //We will overrite it when we configure final web host.
 
-            //Log.Logger = ConfigureSerilog();
+            Log.Logger = ConfigureSerilog(new LoggerConfiguration(), environment).CreateLogger();
+
+            Log.Information("Logging Configured");
 
             CreateHostBuilder(args).Build().Run();
         }
@@ -42,10 +46,71 @@ namespace Sales.Payments
 
             return environment switch
             {
-                //"Development" => ConfigureDevelopmentLogging(config),
+                "Development" => ConfigureDevelopmentLogging(config),
                 "Staging" => config.WriteTo.Console(formatter: new RenderedCompactJsonFormatter()),
+                "Production" => config.WriteTo.Console(formatter: new RenderedCompactJsonFormatter())
+                    .Destructure.UsingAttributes(),
+                _ => throw new InvalidOperationException($"Cannot configure logging,unexpected enviroment '{environment}'.")
 
             };
+        }
+
+        static LoggerConfiguration ConfigureDevelopmentLogging(LoggerConfiguration loggerConfiguration)
+        {
+            var expectedDirectory = Assembly.GetExecutingAssembly().GetName().Name;
+            var currentDirectory = Directory.GetCurrentDirectory();
+
+            //if (!currentDirectory.EndsWith(expectedDirectory!))
+            //{
+            //    throw new InvalidOperationException(
+            //        $"Expected Current Diretory to be '{expectedDirectory}' but it is '{currentDirectory}'"
+            //        );
+            //}
+
+            var logDirectory = Path.Combine(currentDirectory, "logs");
+
+            static bool SourceContextIsEfCommand(LogEvent e)
+            {
+                if (!e.Properties.TryGetValue("SourceContext", out var propertyValue))
+                {
+                    return false;
+                }
+                return propertyValue switch
+                {
+                    ScalarValue { Value: string value } => value.StartsWith(
+                        "Microsoft.EntityFrameworkCore.Database.Command"),
+                    _ => false
+                };
+            }
+
+            static bool LogAllExceptEfCommands(LogEvent e) =>
+                !SourceContextIsEfCommand(e) || e.Level >= LogEventLevel.Warning;
+
+            return loggerConfiguration
+                .WriteTo.Conditional(LogAllExceptEfCommands,
+                writeTo => writeTo.Console(
+                    restrictedToMinimumLevel: LogEventLevel.Information,
+                    outputTemplate:
+                    "[{Timestamp : HH:MM:ss} {Level:u3}] {RequestId} {Message:lj}{NewLine}{Exception}"
+                )
+                )
+                .WriteTo.Conditional(LogAllExceptEfCommands,
+                writeTo => writeTo.File(
+                    Path.Combine(logDirectory,"log.txt"),
+                    outputTemplate:
+                    "[{Timestamp : HH:MM:ss.fff} | {Level:u3}] | {RequestMethod} {RequestPath} | {RequestId} | {SourceContext} | {Message:lj}{NewLine}{Exception}",
+                    rollingInterval: RollingInterval.Day
+                )
+                )
+                .WriteTo.Conditional(SourceContextIsEfCommand,
+                writeTo => writeTo.File(
+                    Path.Combine(logDirectory, "database.txt"),
+                    outputTemplate:
+                    "[{Timestamp : HH:MM:ss.fff} | {Level :u3}] | {RequestMethod} {RequestPath} | {RequestId} | {SourceContext} | {Message:lj}{NewLine}{Exception}",
+                    rollingInterval: RollingInterval.Day
+                )
+                )
+                ;
         }
     }
 }
