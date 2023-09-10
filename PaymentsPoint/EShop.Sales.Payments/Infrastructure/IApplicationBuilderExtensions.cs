ceff10ca07a277e17889ae2bc688eab8eb7c311a
akshayc27 <akshaychacharkar27@gmail.com>
2023-09-10 21:02:41 +0530

    Registered swagger and Mvc.

---
+++ PaymentsPoint/EShop.Sales.Payments/Infrastructure/IApplicationBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace Sales.Payments.WebApi.Infrastructure
+{
+    public static class IApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseSwaggerMiddleware(
+            this IApplicationBuilder app
+            //IApiVersionDescriptionProvider provider
+            )
+        {
+            //enable swagger json generation
+            app.UseSwagger();
+
+            //enable Swagger Web Page
+            app.UseSwaggerUI(
+                setup =>
+                {
+                    setup.DocumentTitle = "Payments Point API";
+                    setup.EnableDeepLinking();
+                    setup.DisplayRequestDuration();
+
+                    //var majorVersions = provider.ApiVersionDescriptions
+                    //.Select(
+                    //    a => a.ApiVersion.MajorVersion ??
+                    //    throw new InvalidOperationException("Major Version is requied.")
+                    //    )
+                    //.Distinct();
+
+                    //foreach (var version in majorVersions)
+                    //{
+                    //    setup.SwaggerEndpoint(
+                    //        $"/swagger/v{version}/swagger.json",
+                    //        $"PaymentsPoint v{version}"
+                    //        );
+                    //}
+                }
+                );
+            return app;
+        }
+
+    }
+}
--- PaymentsPoint/EShop.Sales.Payments/Startup.cs
+++ PaymentsPoint/EShop.Sales.Payments/Startup.cs
@@ -1,56 +1,119 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Newtonsoft.Json.Converters;
+using Sales.Payments.WebApi.Infrastructure;
 
 namespace Sales.Payments
 {
-        public class Startup
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
         {
-            public Startup(IConfiguration configuration)
-            {
-                Configuration = configuration;
-            }
-
-            public IConfiguration Configuration { get; }
-
-            // This method gets called by the runtime. Use this method to add services to the container.
-            public void ConfigureServices(IServiceCollection services)
-            {
-                services.AddControllers();
-                services.AddEndpointsApiExplorer();
-                services.AddSwaggerGen();
+            Configuration = configuration;
         }
 
-            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.RegisterMvc();
+            //services.AddControllers();
+            services.AddApiVersioning();
+            //services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //Called automatically to set up the ASP.NET core request pipeline.
+        public void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env,
+            //IApiVersionDescriptionProvider versionProvider,
+            IServiceProvider provider
+            )
+        {
+            if (env.IsDevelopment())
             {
-                if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+                //app.UseSwagger();
+                //app.UseSwaggerUI();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwaggerMiddleware();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+    }
+
+    public static class IServiceCollectionExtensions
+    {
+        private const string ExpiredTokenHandler = "Token-Expired";
+
+        public static IServiceCollection RegisterMvc(this IServiceCollection services)
+        {
+            services.AddCors(
+
+                options =>
                 {
-                    app.UseDeveloperExceptionPage();
-                    app.UseSwagger();
-                    app.UseSwaggerUI();
-                }
+                    options.AddDefaultPolicy(
 
-                app.UseHttpsRedirection();
-
-                app.UseRouting();
-
-                app.UseAuthorization();
-
-                app.UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllers();
+                        builder =>
+                        {
+                            builder.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .WithExposedHeaders(ExpiredTokenHandler, "WWW-Authenticate");
+                        });
                 });
-            }
+
+            services.AddControllers(
+                options =>
+                {
+
+                    //Prevent validation attributes on odel from affecting model state.
+                    //Doest not affect model binding errors (e.g. malformed json)
+                    options.ModelValidatorProviders.Clear();
+
+                    //When the modl binding source is request body, treat  an empty
+                    //body as an error
+                    options.AllowEmptyInputInBodyModelBinding = false;
+                }
+                )
+                .AddNewtonsoftJson(
+                options =>
+                {
+                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                }
+                )
+                ;
+
+            services.AddApiVersioning(
+                options =>
+                {
+                    options.AssumeDefaultVersionWhenUnspecified = false;
+                    options.RegisterMiddleware = true;
+                    options.UseApiBehavior = true;
+                    //options.ErrorResponses = new();
+                }
+                );
+
+            return services;
+
         }
+    }
 
 }
